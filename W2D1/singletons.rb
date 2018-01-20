42d8f85a97e7abdc1f4b6dbc505f371fa6fc28ea
EMMilco <elliot.milco@gmail.com>
2018-01-20 14:08:32 -0500

    Add notes on Singleton behavior

--- W2D1/singletons.rb
+++ W2D1/singletons.rb
@@ -1,7 +1,13 @@
-# Notes: If you name a script the same as a ruby stdlib file (in this case
+# Notes:
+
+# --- If you name a script the same as a ruby stdlib file (in this case
 # if the file were named 'singleton.rb'), pry will load the core file inst-
 #   ead of the script in the working directory.
-
+#
+# --- Don't forget to require stdlib gems that you need (in this case 'singleton')
+#
+# --- Subclasses of classes that mix-in Singleton are Singletons, but their
+# instances are each unique.
 
 require 'singleton'
 
