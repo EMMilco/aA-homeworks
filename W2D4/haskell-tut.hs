2276d8b48e535650e6ab4ae26da803823c1bab26
EMMilco <elliot.milco@gmail.com>
2018-01-24 22:04:35 -0500

    play around with haskell and ghci

---
+++ W2D4/haskell-tut.hs
@@ -0,0 +1,13 @@
+{- comment
+	-
+multiline comment -}
+
+import Data.List
+import System.IO
+
+sumOfNums = sum [1..10000]
+
+negNumEx = 5 + (-4)
+
+num9 = 9 :: Int
+sqrtOf9 = sqrt (fromIntegral num9)
