b9b1183875d170654c10066325bdcfbd1ae3f1ff
EMMilco <elliot.milco@gmail.com>
2018-01-31 23:36:55 -0500

    Attempted N+1 Buster HWRK

---
+++ W3D4/n_1_buster/app/models/artist.rb
@@ -0,0 +1,20 @@
+class Artist < ApplicationRecord
+  has_many :albums,
+    class_name: 'Album',
+    foreign_key: :artist_id,
+    primary_key: :id
+
+  def n_plus_one_tracks
+    albums = self.albums
+    tracks_count = {}
+    albums.each do |album|
+      tracks_count[album.title] = album.tracks.length
+    end
+
+    tracks_count
+  end
+
+  def better_tracks_query
+    self.albums.joins("join tracks on albums.id = tracks.album_id").select("albums.title as album, COUNT(*)").group('albums.title')
+  end
+end
