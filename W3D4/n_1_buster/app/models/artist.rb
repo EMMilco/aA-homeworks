264129ea99a3afefec9e91ef65f305a991fa385f
EMMilco <elliot.milco@gmail.com>
2018-02-02 08:34:10 -0500

    W3D5 HW

--- W3D4/n_1_buster/app/models/artist.rb
+++ W3D4/n_1_buster/app/models/artist.rb
@@ -15,6 +15,16 @@ class Artist < ApplicationRecord
   end
 
   def better_tracks_query
-    self.albums.joins(:tracks).select("albums.title as album, COUNT(*)").group('albums.title')
+    tracks_count = {}
+    albums = self.albums
+    .joins(:tracks)
+    .select("albums.title, COUNT(*) as track_count")
+    .group('albums.id')
+
+    albums.each { |album_record| puts album_record.instance_variables}
+    tracks_count
+
   end
+
+
 end
