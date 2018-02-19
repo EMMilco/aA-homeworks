881ec114d6e069e56e0b6ed3acbb3fedc5a3a8fd
EMMilco <elliot.milco@gmail.com>
2018-02-19 00:39:35 -0500

    Complete final myBind exercises for understanding

--- W6D1/inheritance_code_along.js
+++ W6D1/inheritance_code_along.js
@@ -34,16 +34,28 @@ Animal.prototype.eat = function () {
 
 
 
-var inherits = function (parent, child) {
-  var Surrogate = function(){};
-  Surrogate.prototype = parent.prototype;
-  child.prototype = new Surrogate();
-  child.prototype.constructor = child;
-};
-var canvas = document.getElementById('canvas'); 
-var c = canvas.getContext('2d'); 
-c.fillStyle = "red"; 
-c.fillRect(100,100,400,300); 
+// var inherits = function (parent, child) {
+//  var Surrogate = function(){};
+//  Surrogate.prototype = parent.prototype;
+//  child.prototype = new Surrogate();
+//  child.prototype.constructor = child;
+//};
+//var canvas = document.getElementById('canvas'); 
+//var c = canvas.getContext('2d'); 
+//c.fillStyle = "red"; 
+//c.fillRect(100,100,400,300); 
 
 
 // REMINDER: The order in which code is loaded matters.  getElementById does not work if the element has not yet been built on the page when the JS script is loaded.
+//
+//
+//
+//
+
+
+
+
+Function.prototype.myBind2 = function (context) {
+  return () => this.apply(context)
+};
+
