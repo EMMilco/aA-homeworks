3f52357a0fa3170740088f1322d338e71dcc14d3
EMMilco <elliot.milco@gmail.com>
2018-02-25 20:39:49 -0500

    W7D1 Homework - Redux

---
+++ W7D1/skeleton/frontend/actions.js
@@ -0,0 +1,13 @@
+
+
+selectCurrency = (baseCurrency, rates) => {
+  return {
+    type: "SWITCH_CURRENCY",
+    baseCurrency,
+    rates
+  };
+};
+
+window.selectCurrency = selectCurrency;
+
+export default selectCurrency;
---
+++ W7D1/skeleton/frontend/store.js
@@ -0,0 +1,6 @@
+import createStore from 'redux';
+import reducer from './reducer.js';
+
+let store = createStore(Reducer);
+
+export default store;
