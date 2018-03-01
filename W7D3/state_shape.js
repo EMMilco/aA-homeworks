357fb615205e4c4a4b4225360654a7aded49eb90
EMMilco <elliot.milco@gmail.com>
2018-03-01 00:18:21 -0500

    update state-shape draft

---
+++ W7D3/state_shape.js
@@ -0,0 +1,107 @@
+{
+  ents: {
+    users: {
+      244: {
+        id: 244,
+        full_name: "Bob Robertson",
+        bio: "",
+        avatar_url: "http://www.unsplash.com/fakeimage.png",
+        creation_date: 2018-02-27
+        },
+
+      417: {
+        id: 417,
+        full_name: "Sarah Gwondon",
+        bio: "",
+        avatar_url: "http://www.unsplash.com/fakeimage.png",
+        creation_date: 2018-02-27
+        },
+
+      379: {
+        id: 379,
+        full_name: "Rex Farley",
+        bio: "",
+        avatar_url: "http://www.unsplash.com/fakeimage.png",
+        creation_date: 2018-02-27
+        }
+      },
+
+    articles: {
+      1934: {
+        id: 1934,
+        title: "Why gondolas are on the wane in America",
+        author_id: 379,
+        featured_image_url: "http://www.unsplash.com/fakeimage.png",
+        publish_date: 2018-02-27
+        },
+
+      2343: {
+        id: 2343,
+        title: "How nouns became verbs",
+        author_id: 379,
+        featured_image_url: "http://www.unsplash.com/fakeimage.png",
+        publish_date: 2018-02-27
+        },
+
+      5464: {
+        id: 5464,
+        title: "American Apocalypse: The Decline of US Steel",
+        author_id: 379,
+        featured_image_url: "http://www.unsplash.com/fakeimage.png",
+        publish_date: 2018-02-27
+        }
+      },
+
+    chunks: {
+      4131: {
+        id: 4131,
+        text: "I saw the best minds of my generation destroyed by madness, starving, hysterical, naked, wandering through the streets at dawn searching for an angry fix",
+        chunkable_id: 31949,
+        content_type: "paragraph"
+        ord: 1
+        },
+
+      3242: {
+        id: 3242,
+        url: "http://www.unsplash.com/fakeimage.png",
+        chunkable_id: 2343,
+        content_type: "image"
+        ord: 2
+        },
+
+      4565: {
+        id: 4565,
+        chunkable_id: 2343,
+        content_type: "divider"
+        ord: 5
+        }
+      },
+
+    comments: {
+      31949: {
+        id: 31949,
+        title: "Why gondolas are on the wane in America",
+        author_id: 379,
+        publish_date: 2018-02-27
+        },
+
+      42333: {
+        id: 42333,
+        title: "How nouns became verbs",
+        author_id: 379,
+        parent_id: 31949,
+        publish_date: 2018-02-27
+        },
+
+      65444: {
+        id: 65444,
+        title: "American Apocalypse: The Decline of US Steel",
+        author_id: 379,
+        publish_date: 2018-02-27
+        }
+      }
+    },
+
+  ui: {},
+  errors: {} ,
+}
